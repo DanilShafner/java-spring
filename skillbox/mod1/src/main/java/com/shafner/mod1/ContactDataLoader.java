7595dbfd7298a67139719f351e9aae79e0ca65f5
Danil Shafner <126704160+DanilShafner@users.noreply.github.com>
2024-10-30 21:48:11 +0500

    Delete skillbox/mod1 directory

--- skillbox/mod1/src/main/java/com/shafner/mod1/ContactDataLoader.java
+++
@@ -1,8 +0,0 @@
-package com.shafner.mod1;
-
-import java.io.IOException;
-import java.util.HashMap;
-
-public interface ContactDataLoader {
-    HashMap<String, ContactDetails> fetchContacts() throws IOException;
-}
