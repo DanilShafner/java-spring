11fa8b5f656281ebd9c1762481d4cb62302c6495
Danil Shafner <7eleven12@mail.ru>
2024-12-20 19:53:07 +0500

    mod6

---
+++ skillbox/mod6/src/main/java/ru/shafnerdanil/SkillBoxSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.shafnerdanil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+public class SkillBoxSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SkillBoxSpringApplication.class, args);
+    }
+}
---
+++ skillbox/mod6/src/main/java/ru/shafnerdanil/skillbox/mod6/dto/OrderEvent.java
@@ -0,0 +1,13 @@
+package ru.shafnerdanil.skillbox.mod6.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderEvent {
+    private String product;
+    private Integer quantity;
+}
